a96037be16324dcefc2c6c8f1a773a0803c7a735
Fabricio Diaz <diazfn@MUNINQN.GOV.AR>
2026-03-04 08:21:13 -0300

    Agregue el footer

--- ceramicka-ecommerce/src/components/shared/Footer.tsx
+++ ceramicka-ecommerce/src/components/shared/Footer.tsx
@@ -1,8 +1,9 @@
 import { BiChevronRight } from "react-icons/bi"
 import { Link } from "react-router-dom"
+import { socialLinks } from "../../constants/links"
 
 export const Footer = () => {
-    return 
+    return (
         <footer className="py-16 bg-gray-950 px-12 flex justify-between gap-10 text-sm text-slate-200 flex-wrap
         md:flex-nowrap">
             <Link to="/" className={`text-2xl font-bold tracking-tighter transition-all text-white flex-1`}>
@@ -43,7 +44,24 @@ export const Footer = () => {
                     No te pierdas las nuevas piezas!
                 </p>
 
-                <div className="flex"></div>
+                <div className="flex">
+                    {
+                        socialLinks.map((link) => (
+                            <a
+                                key={link.id}
+                                href={link.href}
+                                target="_blank" 
+                                rel="noreferrer"
+                                className="text-slate-300 border border-gray-800 w-full h-full py-3.5 flex items-center justify-center transition-all
+                                 hover:bg-white hover:text-gray-950 "
+                            >
+
+                            </a>
+                        ))
+                    }
+                </div>
             </div>
         </footer>
+    )
+        
 }
--- ceramicka-ecommerce/src/constants/links.tsx
+++ ceramicka-ecommerce/src/constants/links.tsx
@@ -1,5 +1,8 @@
 //import { href } from "react-router-dom";
 
+//import { title } from "process"
+import { FaInstagram } from "react-icons/fa6"
+
 export const navbarLinks = [
     {
         id: 1,
@@ -16,4 +19,26 @@ export const navbarLinks = [
         title: "sobremi",
         href: "/sobremi",
     },
+]
+
+export const socialLinks = [
+    {
+        id: 1, 
+        title: "Instagram",
+        href: "https://www.instagram.com/ceramicka.arg/",
+        icon: <FaInstagram size={20} />
+    },
+    {
+        id: 2, 
+        title: "Facebook",
+        href: "https://www.facebook.com/ceramicka.arg",
+        icon: <FaInstagram size={20} />
+    },
+    {
+        id: 3, 
+        title: "Whatsapp",
+        href: "https://wa.me/5491134567890",
+        icon: <FaInstagram size={20} />
+    }
+
 ]
