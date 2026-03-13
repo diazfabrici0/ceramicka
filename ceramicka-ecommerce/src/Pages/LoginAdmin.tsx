e80a139440d031bbb62190e82eade86b21353b59
Fabricio Diaz <diazfn@MUNINQN.GOV.AR>
2026-03-13 08:01:05 -0300

    comence el loginAdmin.tsx

--- ceramicka-ecommerce/src/Pages/LoginAdmin.tsx
+++ ceramicka-ecommerce/src/Pages/LoginAdmin.tsx
@@ -1,3 +1,7 @@
+import React,  { useState } from  'react';
+import { useNavigate } from 'react-router-dom';
+import { useAuth } from '../../src/context/AuthContext'  
+
 export const LoginAdmin = () => {
     return (
         <div>login admin</div>
