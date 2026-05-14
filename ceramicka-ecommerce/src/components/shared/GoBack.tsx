874fa9809e953d744d377093e007840fe5bf83e2
diazfabrici0 <dfabricio255@gmail.com>
2026-05-14 10:44:32 -0300

    retoques

--- ceramicka-ecommerce/src/components/shared/GoBack.tsx
+++ ceramicka-ecommerce/src/components/shared/GoBack.tsx
@@ -7,7 +7,7 @@ export const GoBack = () => {
     return (
         <button
             onClick={() => navigate(-1)}
-            className="fixed top-20 left-4 z-40 bg-white/80 backdrop-blur-sm hover:bg-white md:p-1 p-1 rounded shadow-sm transition-all border border-gray-200 text-gray-700 hover:text-[#f880b8]"
+            className=" top-20 left-4 z-40 bg-white/80 backdrop-blur-sm hover:bg-white md:p-1 p-1 rounded shadow-sm transition-all border border-gray-200 text-gray-700 hover:text-[#f880b8]"
             aria-label="Volver atrás"
         >
             <ArrowLeft size={22} />
